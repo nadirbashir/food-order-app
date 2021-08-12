749cc3aa3fba37ca9d65b16bd9416e3a3e9b2d82
nadirbashir <1.nadirbashir@gmail.com>
2021-08-13 02:50:12 +0500

    MealItemForm complete

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,12 +1,18 @@
 import classes from "./MealItem.module.css";
+import { MealItemForm } from "./MealItemForm";
 
 export const MealItem = ({ meal }) => {
-    const price = `$${meal.price}`;
+  const price = `$${meal.price}`;
   return (
     <li className={classes.meal}>
-      <h3>{meal.name}</h3>
-      <div className={classes.description}>{meal.description}</div>
-      <div className={classes.price}>{price}</div>
+      <div>
+        <h3>{meal.name}</h3>
+        <div className={classes.description}>{meal.description}</div>
+        <div className={classes.price}>{price}</div>
+      </div>
+      <div>
+        <MealItemForm />
+      </div>
     </li>
   );
 };
